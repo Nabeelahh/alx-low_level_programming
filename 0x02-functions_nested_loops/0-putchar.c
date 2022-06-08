c1c3331e542032b755c148592dc032aa66e21fa3
Nabeelahh <saninabeelah@gmail.com>
2022-06-08 04:42:19 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,12 @@
 
-
-
-
 #include "main.h"
 
-
-
-
-
 /**
- 
  *  main - Entry point
  *  Return: nothing, return void
- 
  */
-
-
-
 int main(void)
-
 {
-
 	_putchar('_');
 
 	_putchar('p');
@@ -38,8 +24,6 @@ int main(void)
 	_putchar('r');
 
 	_putchar('\n');
-
-
 
 	return (0);
 
