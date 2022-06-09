f3a2e722a8108bab3c5d001ef50e86cad618d29a
Nabeelahh <saninabeelah@gmail.com>
2022-06-09 00:58:34 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * main-tests functions that print either positive or negetive
+ * return:0
+ */
+int main(void)
+
+{
+	int i;
+	i = 0;
+	positive_or_negetive(i);
+
+	return (0);
+}
+
